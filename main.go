1330a2ff79aadbc7d54d356e2a7397733665212c
Harsimranjit Dhaliwal <dhaliwal-h@outlook.com>
2022-07-09 20:55:46 -0400

    Initial Commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Print("Starting Server at 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request Succssful\n")
+	name := r.FormValue("name")
+	fmt.Fprintf(w, "Name is %s\n", name)
+
+}
